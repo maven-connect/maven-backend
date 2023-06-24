6cbb436ee2728bce053991a8c33787219207a740
Chinmay J <86140365+JawHawk@users.noreply.github.com>
2023-06-24 22:39:27 +0530

    Revert "deployment changes"

--- server/settings.py
+++ server/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
-import os
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,7 +84,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
