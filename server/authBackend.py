2586c1fd7dac9228a98a5c373887ea47e93a6bc3
jawhawk <mavenal2143@gmail.com>
2023-06-26 14:23:32 +0530

    fix: login auth backend bug

--- server/authBackend.py
+++ server/authBackend.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
 
-class googleAuthBackend(ModelBackend):
+class googleAuthBackend(BaseBackend):
 
     def authenticate(self, request, email=None, **kwargs):
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(username=email)
+            user = UserModel.objects.get(email=email)
             return user
         except UserModel.DoesNotExist:
             return None
@@ -18,11 +18,11 @@ class googleAuthBackend(ModelBackend):
         except UserModel.DoesNotExist:
             return None
 
-class EmailPasswordBackend(ModelBackend):
+class EmailPasswordBackend(BaseBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(username=email)
+            user = UserModel.objects.get(email=email, password=password)
         except UserModel.DoesNotExist:
             return None
         else:
@@ -31,6 +31,8 @@ class EmailPasswordBackend(ModelBackend):
 
     def get_user(self, user_id):
         UserModel = get_user_model()
+        print(user_id, 'email')
+
         try:
             return UserModel.objects.get(pk=user_id)
         except UserModel.DoesNotExist:
--- users/models.py
+++ users/models.py
@@ -1,10 +1,22 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from django.db import models  
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone  
+from django.utils.translation import gettext_lazy as _  
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):  
+    username = None  
+    email = models.EmailField(_('email_address'), unique=True, max_length = 200)  
+    date_joined = models.DateTimeField(default=timezone.now)  
+    is_staff = models.BooleanField(default=False)  
+    is_active = models.BooleanField(default=True)  
     is_verified = models.BooleanField(default=False)
     batch = models.IntegerField(null=True, blank=True)
-    
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     branches = [
         ("CS", "Computer Science"),
         ("ECE", "Electronics"),
@@ -13,3 +25,16 @@ class CustomUser(AbstractUser):
         ("DS", "Design"),
     ]
     branch = models.CharField(choices=branches, null=True, max_length=4, blank=True)
+
+    def has_perm(self, perm, obj=None):  
+        return True  
+  
+    def is_staff(self):  
+         return self.staff  
+    
+    @property  
+    def is_admin(self):  
+        return self.admin  
+  
+    def __str__(self):  
+        return self.email  
