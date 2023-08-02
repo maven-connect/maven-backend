1eaf9431abcd55587b403c52f13284c5d2b09525
Chinmay Joshi <mavenal2143@gmail.com>
2023-08-03 00:26:02 +0530

    postgreSQL

--- server/settings.py
+++ server/settings.py
@@ -97,8 +97,12 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mavendb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
